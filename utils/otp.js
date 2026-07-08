6a00892cf6a3315e2d836eb64166dc25c1f39fbb
Arpit Menaria <menariaarpit2@gmail.com>
2026-07-08 11:24:38 +0530

    ASDAS

--- utils/otp.js
+++ utils/otp.js
@@ -4,9 +4,8 @@ const OTP_LENGTH = 4;
 const OTP_TTL_SECONDS = 600;
 
 const generateOtp = () => {
-  const min = 10 ** (OTP_LENGTH - 1);
-  const max = 10 ** OTP_LENGTH - 1;
-  return String(Math.floor(min + Math.random() * (max - min + 1)));
+  // TODO: revert to random OTP before production launch
+  return "1234";
 };
 
 const hashOtp = async (otp) => bcrypt.hash(otp, 10);
